c4caea34f6a7eefa00c09e0903777f97eb24c41a
tobam <tobamohaqqeq84@gmail.com>
2025-11-29 10:20:55 +0000

    8th

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -3,8 +3,24 @@
 //Lab 36
 
 #include <iostream>
+#include <string>
+#include <fstream>
 #include "IntBinaryTree.h"
 using namespace std;
+
+void loadCodesFromFile(string filename, IntBinaryTree &tree);
+if(!inFile) {
+    cout << "Error opening file." << endl;
+    return;
+}
+    string code;
+
+    while (inFile >> code){
+        tree.insertNode(code);
+    }
+inFile.close():
+    cout << "Finished loading codes from " << filename << endl;
+}
 
 int main() {
     IntBinaryTree tree;
