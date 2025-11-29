6eda6e8bb9a0e26f7686cccead235eb21bd7f6c2
tobam <tobamohaqqeq84@gmail.com>
2025-11-29 09:20:53 +0000

    Milestone 1 compeleted

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+//Toba Mohaqqeq
+//COMSC-210
+//Lab 36
+
 #include <iostream>
 #include "IntBinaryTree.h"
 using namespace std;
+
 int main() {
+    IntBinaryTree tree;
+
+    cout << "Lab 36 project is set up and running successfully!" << endl;
 return 0;
 }
