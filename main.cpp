638b516d2e21943ecc9ac5b2f3254c674e7e27ba
tobam <tobamohaqqeq84@gmail.com>
2025-11-29 10:40:46 +0000

    10th

--- main.cpp
+++ main.cpp
@@ -1,35 +1,30 @@
-//Toba Mohaqqeq
-//COMSC-210
-//Lab 36
+// Toba Mohaqqeq
+// COMSC-210
+// Lab 36
 
+#include "IntBinaryTree.h"
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
-#include "IntBinaryTree.h"
 using namespace std;
 
 void loadCodesFromFile(const string &filename, IntBinaryTree &tree){
-ifstream inFile(filename);
-if(!inFile) {
-    cerr << "Error opening file." << endl;
-    return;
-}
-    string code;
-
-    while (inFile >> code){
-        tree.insertNode(code);
-    }
-inFile.close();
+    ifstream inFile(filename);  
+if(!inFile) {        
+cerr << "Error opening file." << endl;         return;     }   
+    string code;     
+    while (inFile >> code){         tree.insertNode(code);     }   
+    inFile.close();     
     cout << "Finished loading codes from " << filename << endl;
 }
 
 int main() {
-    IntBinaryTree tree;
+  IntBinaryTree tree;
 
-    loadCodesFromFile("codes.txt", tree);
-    
-    cout << "BST contents from codes.txt" << endl;
-    tree.displayInOrder();
-    
-return 0;
+  loadCodesFromFile("codes.txt", tree);
+
+  cout << "BST contents from codes.txt" << endl;
+  tree.displayInOrder();
+
+  return 0;
 }
